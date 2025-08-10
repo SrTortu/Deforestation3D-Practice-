60c6e50ab563713c49c3640dc09fba692e0a2b04
DESKTOP-VMTN4KT\SrTortuga <santiagogc0828@gmail.com>
2025-08-10 19:53:23 +0200

    Main menu buttos now have sound when they are clicked and fixed buttons behaviour

--- Assets/Scripts/UI/MainMenu/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenu/MainMenuController.cs
@@ -10,22 +10,27 @@ using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
-    [Header("Buttons")]
-    [SerializeField] private Button _playButton;
+    [Header("Main Panel Menu")] [Space(20)] [SerializeField]
+    private GameObject _mainPanel;
+
+    [Header("Buttons")] [SerializeField] private Button _playButton;
     [SerializeField] private Button _settingsButton;
     [SerializeField] private Button _exitButton;
-    [Space(20)]
-    [Header("Sound")]
-    [SerializeField] private AudioMixer _audioMixer;
+
+
+    [Space(20)] [Header("Sound")] [SerializeField]
+    private AudioMixer _audioMixer;
+    [SerializeField] private AudioSource _buttonClickSound;
+    [SerializeField] private AudioSource _buttonEnterSound;
+
     [SerializeField] private Slider _musicSlider;
     [SerializeField] private Slider _fxSlider;
-    [Space(20)]
-    [Header("Settings")]
-    [SerializeField] private Button _settingsOkButton;
+
+    [Space(20)] [Header("Settings")] [SerializeField]
+    private Button _settingsOkButton;
+
     [SerializeField] private GameObject _settingsPanel;
-    
-    
-    
+
 
     private void Start()
     {
@@ -39,15 +44,20 @@ public class MainMenuController : MonoBehaviour
 
     private void PlayButtonOnClick()
     {
+        _buttonClickSound.Play();
         SceneManager.LoadScene(1);
     }
 
     private void SettingsButtonOnClick()
     {
+        _buttonClickSound.Play();
+        _settingsPanel.SetActive(true);
+        _mainPanel.SetActive(false);
     }
 
     private void ExitButtonOnClick()
     {
+        _buttonClickSound.Play();
         Application.Quit();
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
@@ -57,13 +67,17 @@ public class MainMenuController : MonoBehaviour
     private void SetFxVolume(float volume)
     {
         _audioMixer.SetFloat("FxVolume", volume);
-    } private void SetMusicVolume(float volume)
+    }
+
+    private void SetMusicVolume(float volume)
     {
         _audioMixer.SetFloat("MusicVolume", volume);
     }
 
     private void CloseSettingsPanel()
     {
+        _buttonClickSound.Play();
         _settingsPanel.SetActive(false);
+        _mainPanel.SetActive(true);
     }
 }
